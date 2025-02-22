6a5cddcb5c3ee2951aad250f0031c9c059e22b78
Borong <47046348+borongzhang@users.noreply.github.com>
2025-02-22 05:35:11 -0600

    minor

--- back_projection_diffusion/src/__init__.py
+++ back_projection_diffusion/src/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Borong Zhang.
+# Copyright 2024 Borong Zhang
 #
 # Licensed under the MIT License (the "License");
 # you may not use this file except in compliance with the License.
